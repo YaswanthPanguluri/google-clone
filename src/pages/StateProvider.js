599b256c057d4e0e43cd0ee4637959b1e564e41a
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-09-04 00:10:10 -0500

    1:43:21

---
+++ src/pages/StateProvider.js
@@ -0,0 +1,14 @@
+import React,{createContext,useContext,useReducer} from "react";
+
+//Preparing the data layer
+export const StateContext = createContext();
+
+
+export const StateProvider = ({reducer,initialState,children})=>(
+    <StateContext.Provider value={useReducer(reduce,initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+//Hook alows us to pull information from data layer
+export const useStateValue = () => useContext(StateContext);
