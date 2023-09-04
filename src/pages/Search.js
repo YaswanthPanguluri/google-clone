eb7a3befa8a76ac710349b0cda51a922d413e457
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-09-03 23:58:10 -0500

    Search component completed

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,32 +3,51 @@ import "./Search.css";
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
 import { Button } from "@mui/material";
-function Search() {
+//import { useHistory } from "react-router-dom";   
+import {useNavigate} from 'react-router-dom';
+
+function Search({hideButtons = false}) {
 const[input,setInput] = useState("");
+//const history = useHistory();  
+const navigate = useNavigate();
 
     const search = e =>{
         e.preventDefault();
-        console.log("You hit Search"); //Remove
-    }
+        console.log("You hit Search >>",input); //Remove
+        // Do some thing with input
+        // come back and fix it
+        //history.push("/Search");
+        navigate("/Search");
+
+    };
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input
-          value={input} 
+          value={input}
           onChange={(e) => {
             setInput(e.target.value);
           }}
         />
         <MicIcon />
       </div>
-      <div className="search_buttons">
-        <Button variant="outline" onClick={search}>
-          Google Search
-        </Button>
-        <Button variant="outline">I'am Feeling Lucky</Button>
-      </div>
-    </div>
+      {!hideButtons ? (
+        <div className="search_buttons">
+          <Button type="submit" variant="outline" onClick={search}>
+            Google Search
+          </Button>
+          <Button variant="outline">I'am Feeling Lucky</Button>
+        </div>
+      ) : (
+        <div className="search_buttons">
+          <Button className = "search_buttonsHidden" type="submit" variant="outline" onClick={search}>
+            Google Search
+          </Button>
+          <Button className = "search_buttonsHidden" variant="outline">I'am Feeling Lucky</Button>
+        </div>
+      )}
+    </form>
   );
 }
 
